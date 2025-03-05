3a50878bf023629d1bf2efca66e2c742a9270bf7
David Santiago <ds.tapias@uniandes.edu.co>
2025-03-04 21:35:15 -0500

    conexion del back con el front

--- last-bite-front/src/App.js
+++ last-bite-front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/data`)
+            .then(response => setData(response.data.message))
+            .catch(error => console.error("Error fetching data:", error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Flask + React</h1>
+            <p>{data || "Loading..."}</p>
+        </div>
+    );
 }
 
 export default App;
